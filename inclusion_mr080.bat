875a781d529abcf3248784e032e53aa02da22c41
BerryWei <54433390+BerryWei@users.noreply.github.com>
2024-11-10 18:44:05 +0800

    Remove inclusion_mr060.bat and inclusion_mr065.bat from tracking

--- inclusion_mr080.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-python D:\Material_mask_autoencoder\main_pretrain.py ^
-    --data_path D:\2d_composite_mesh_generator\inclusion_train_100k ^
-    --mask_ratio 0.80 ^
-    --output_dir D:\Material_mask_autoencoder\output_dir\inclusion_mr080 ^
-    --log_dir    D:\Material_mask_autoencoder\output_dir\inclusion_mr080
-
-
